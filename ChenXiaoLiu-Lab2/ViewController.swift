afd5035e72871a05111c8cf720b5cafea81c20ed
ChenXiao Liu <810196813@qq.com>
2021-09-22 21:53:21 -0500

    setup

--- ChenXiaoLiu-Lab2/ViewController.swift
+++ ChenXiaoLiu-Lab2/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var happinessBar: DisplayView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func playButtonPressed(_ sender: Any) {
+        happinessBar.animateValue(to: CGFloat(0.5))
+    }
 }
 
